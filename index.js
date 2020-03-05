8529bce393a5f1f263e35105349ebc1f1699a774
rshiddapur <rshiddapur@gmail.com>
2020-03-05 16:23:12 -0500

    Changed code to blink all LEDs

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var five =require('johnny-five'); //require/import the johnny-five module
 var board=new five.Board(); //the Board class from johnny-five module
 
-//When the board is ready create a led object on pin 13 and blink it 
-//every 500 milliseconds.
+//When the board is ready strobe white every 500 milliseconds.
 
 board.on('ready',function(){
     var RedLED=new five.Led(9);
-    var BlueLED=new five.Led(11);
     var GreenLED=new five.Led(10);
-    RedLED.blink(500);}); 
+    var BlueLED=new five.Led(11);
+    
+    RedLED.strobe(500);
+	GreenLED.strobe(500); 
+	BlueLED.strobe(500);
+}); 
