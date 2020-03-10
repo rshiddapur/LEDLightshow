f3d0764b19f814c2a9a3179eccd77db2279ca676
rshiddapur <rshiddapur@gmail.com>
2020-03-10 18:37:45 -0400

    Updated index.js file to use Johnny Five's LED RGB API rather than controlling LEDs individually

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var five =require('johnny-five'); //require/import the johnny-five module
 var board=new five.Board(); //the Board class from johnny-five module
 
-//When the board is ready strobe white every 500 milliseconds.
+//When the board is ready strobe every 500 milliseconds.
 
-board.on('ready',function(){
-    var RedLED=new five.Led(9);
-    var GreenLED=new five.Led(10);
-    var BlueLED=new five.Led(11);
-    
-    RedLED.strobe(500);
-	GreenLED.strobe(500); 
-	BlueLED.strobe(500);
+// If individual control is needed 
+var RedLED;
+var GreenLED;
+var BlueLED;
+
+// Controls the strip as an RGB LED
+var LightStrip;
+
+board.on('ready', function(){
+
+	// Definitions for individual LEDs
+	RedLED = new five.Led(9);
+	GreenLED = new five.Led(10);
+	BlueLED = new five.Led(11);
+
+	LightStrip = new five.Led.RGB([9, 10, 11]);
+
+	LightStrip.off();
+
+	//LightStrip.color(0, 255, 255);
+	LightStrip.color("darkgreen");
+
+	LightStrip.strobe(500);
+
 }); 
